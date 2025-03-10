11d59add1f602d4b89ce1803b7408fd9444fd94b
DennisKlingener <dennisklingener@gmail.com>
2025-03-10 12:47:37 -0400

    revert

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import '../styles/kjh'
+import '../styles/Login.css'
 
 
 // FOR THE LOGIN PAGE WE CAN HAVE ONE ROW WITH ONE COLUMN WITH TWO DIVS IN IT.
