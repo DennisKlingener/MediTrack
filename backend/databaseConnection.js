1c82f8ca4fbf35d1d4ea69e56044fe26942b95c8
DennisKlingener <dennisklingener@gmail.com>
2025-03-16 11:16:08 -0400

    Implemented a connection pool to resolve SQL connection timeouts

--- backend/databaseConnection.js
+++ backend/databaseConnection.js
@@ -1,22 +1,17 @@
 require("dotenv").config();
 const mySQL = require("mysql2");
 
-// Create a connection for the database.
-const database = mySQL.createConnection({
+// A connection pool automatically manages "connectionLimit" connections to the databse
+// and handles recreating new connections when old ones timeout.
+const databasePool = mySQL.createPool({
+    connectionLimit: 10,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,  // Make a user that is not root...
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-});
-
-// Connect to the database.
-database.connect((err) => {
-    if (err) {
-        console.error("Failure to connect to database: ", err.stack);
-        return;
-    }
-    console.log("Successfully connected to database.");
+    waitForConnections: true,
+    queueLimit: 0,
 });
 
 // Export the database connection for use in the endpoints.
-module.exports = database;
+module.exports = databasePool;
