ee05098370b3c7ea9b74b8316c52b5121b8df4c1
JeanP <jean.pierre900014@gmail.com>
2025-04-09 17:51:04 -0400

    Update firebaseAuth.js

--- backend/routes/firebaseAuth.js
+++ backend/routes/firebaseAuth.js
@@ -1,7 +1,8 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {getAuth} from 'firebase/auth';
+// src/firebase.js
+const firebase = require('firebase/app');  // Firebase app initialization
+require('firebase/auth');  // Firebase Authentication SDK
 
+// Replace with your Firebase project's configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCMEQtFODLC9rZwdkT_mb3F1Gusu_1IU-g",
   authDomain: "meditrack-dualauth.firebaseapp.com",
@@ -11,6 +12,12 @@ const firebaseConfig = {
   appId: "1:947596501774:web:f381a371c54a5830aa2d5f",
   measurementId: "G-K3J4LGNFMS"
 };
- 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// Export the auth service for use in components
+module.exports = {
+  auth: firebaseApp.auth(),  // Firebase Authentication service
+  firebaseApp,  // The initialized Firebase app
+};
