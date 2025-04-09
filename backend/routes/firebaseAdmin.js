96a44b3e476d1c703086d9a50e169d223796994c
JeanP <jean.pierre900014@gmail.com>
2025-04-09 18:26:06 -0400

    Create firebaseAdmin.js

---
+++ backend/routes/firebaseAdmin.js
@@ -0,0 +1,10 @@
+const admin = require('firebase-admin');
+
+// Initialize Firebase Admin with the service account key
+const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT);  // Correct path
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+module.exports = admin;
