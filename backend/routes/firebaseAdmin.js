d2eabfd3d53dd06e201cb4d7de6801429dab8678
JeanP <jean.pierre900014@gmail.com>
2025-04-11 12:09:33 -0400

    Update firebaseAdmin.js

--- backend/routes/firebaseAdmin.js
+++ backend/routes/firebaseAdmin.js
@@ -3,7 +3,7 @@ const admin = require('firebase-admin');
 // Decode the Base64 encoded service account key string
 const serviceAccountBase64 = process.env.FIREBASE_SERVICE_ACCOUNT;
 const serviceAccountJson = Buffer.from(serviceAccountBase64, 'base64').toString('utf-8');
-
+ 
 // Parse the decoded JSON string to a JavaScript object
 const serviceAccount = JSON.parse(serviceAccountJson);
 
